a7b821aa9f4442b9e44543fdf4845496f43bb9f5
junsu <junsu0529@gmail.com>
2026-02-10 23:17:04 +0900

    whydonotdeployed

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,9 +1,9 @@
 // 이 파일은 빌드 시마다 자동으로 업데이트됩니다
-// Build Time: 2026-02-10T13:53:24.868Z
+// Build Time: 2026-02-10T14:16:07.723Z
 export const APP_VERSION = {
-  version: '1770731604868',
-  buildTime: '2026-02-10T13:53:24.868Z',
-  hash: 'nh815yygnge'
+  version: '1770732967723',
+  buildTime: '2026-02-10T14:16:07.723Z',
+  hash: 'h0chrk0s0uj'
 };
 
 export default APP_VERSION;
