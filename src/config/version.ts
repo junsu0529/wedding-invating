f3441d9eae14f68d715436d597ae3ad6de31db2b
junsu <junsu0529@gmail.com>
2026-02-10 23:27:24 +0900

    update next

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,9 +1,9 @@
 // 이 파일은 빌드 시마다 자동으로 업데이트됩니다
-// Build Time: 2026-02-10T14:16:07.723Z
+// Build Time: 2026-02-10T14:21:58.918Z
 export const APP_VERSION = {
-  version: '1770732967723',
-  buildTime: '2026-02-10T14:16:07.723Z',
-  hash: 'h0chrk0s0uj'
+  version: '1770733318918',
+  buildTime: '2026-02-10T14:21:58.918Z',
+  hash: 'v7ch2bjomy'
 };
 
 export default APP_VERSION;
